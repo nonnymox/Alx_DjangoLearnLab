28ca4ea6f6e8e30723b24fc1adc21230e680f098
nonnymox <knonxho@gmail.com>
2024-11-10 22:28:08 +0100

    Performed Role Based Auth

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,8 @@
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 from django.contrib.auth import login, logout, authenticate
@@ -32,3 +34,24 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome, Admin!")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome, Librarian!")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome, Member!")
