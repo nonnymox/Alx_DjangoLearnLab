8501080d710dc9c1135d67f25f513aee756c53b8
nonnymox <knonxho@gmail.com>
2024-12-01 22:30:14 +0100

    Integrated api filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,11 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import from rest_framework.filters import OrderingFilter, SearchFilter
+from django_filters import rest_framework
 from rest_framework.filters import OrderingFilter, SearchFilter
-
-
-
 
 
 # ListView: Retrieve all books
