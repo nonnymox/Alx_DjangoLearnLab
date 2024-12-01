fef60d7b8fdf8b78c146c8a4fa50de7ccacbf56d
nonnymox <knonxho@gmail.com>
2024-12-01 22:26:01 +0100

    Integrated api filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,19 +1,22 @@
-from rest_framework.filters import OrderingFilter, SearchFilter
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
-from .filters import BookFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
+
+
+# ListView: Retrieve all books
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_class = BookFilter
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [rest_framework.DjangoFilterBackend]
+    filterset_class = BookFilter  # Open to all users
     search_fields = ['title', 'author']
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']  # Default ordering by title
- # Default ordering by title
     def get_queryset(self):
         queryset = Book.objects.all()
         year = self.request.query_params.get('year')
