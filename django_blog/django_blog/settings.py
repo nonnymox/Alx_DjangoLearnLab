e105ddc8454dc8dc9b6d5e5a86a5d05a99f61ab6
nonnymox <knonxho@gmail.com>
2024-12-13 02:38:45 -0800

    Added the sql redirection and sql file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_db',
+        'USER': 'root',
+        'PASSWORD': 'on2GREATNE$$',
+        'HOST': 'localhost',  # Or the IP of the MySQL server
+        'PORT': '3306',       # Default MySQL port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
+
 
 
 # Password validation
