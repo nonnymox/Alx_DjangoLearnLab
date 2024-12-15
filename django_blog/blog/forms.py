865a8e4978ad84576f71a5df24739aa512d7c83c
nonnymox <knonxho@gmail.com>
2024-12-15 01:58:08 -0800

    Blog Post Management

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from django_blog.blog.models import Post
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
@@ -14,3 +16,8 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,14 @@
 # blog/urls.py
 from django.urls import path
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 
 urlpatterns = [
     path('', views.home, name='home'),  # Root path
@@ -8,4 +16,9 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
