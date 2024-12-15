e3b5a4ec387153af98d065bbb492d2e4cc0fe0b9
nonnymox <knonxho@gmail.com>
2024-12-15 13:31:36 -0800

    Added comment functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:post_id>/comment/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
